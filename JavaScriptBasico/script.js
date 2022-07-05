55e74c8e0723878d0e2a126bd2bbdbc516eb5e00
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-07-05 13:24:37 -0300

    Início da parte de Javascript Básico

---
+++ JavaScriptBasico/script.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector("nav");
+const produtos = document.querySelector(".produtos a");
+console.log(nav);
+console.log(produtos);
+
+console.log(produtos.href);
+
+nav.style.backgroundColor = "black";
+
+nav.style.padding = "1rem";
+
+nav.classList.add("ativo");
+
+
